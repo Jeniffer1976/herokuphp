6906cc9015cb50bc6e080e9b7b13f3b81563f4bf
Jeniffer1976 <21025290@myrp.edu.sg>
2023-01-05 15:02:39 +0800

    Test fixed errors

--- web/test.php
+++ web/test.php
@@ -1,7 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
 <html>
+
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Testing Hello World">
+    <title>Hello World</title>
 </head>
+
 <body>
-  Hello World! :)
+    Hello World! :)
 </body>
+
 </html>
+
+
