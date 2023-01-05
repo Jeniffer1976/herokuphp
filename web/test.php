e26a7b0e2709ffe22ef429de46599fc440d40921
Jeniffer1976 <21025290@myrp.edu.sg>
2023-01-05 19:12:08 +0800

    Error comments

--- web/test.php
+++ web/test.php
@@ -1,10 +1,19 @@
+<!-- fix : Page lacks the HTML doctype -->
 <!DOCTYPE html>
+
+<!-- fix : <html> element does not have a [lang] attribute -->
 <html lang="en">
+
 <html>
 
 <head>
+    <!-- fix : Does not have a <meta name="viewport"> tag with width or initial-scale -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- fix : Document does not have a meta description -->
     <meta name="description" content="Testing Hello World">
+
+    <!-- fix : Document doesn't have a <title> element -->
     <title>Hello World</title>
 </head>
 
@@ -13,5 +22,6 @@
 </body>
 
 </html>
+
 
 
